1d6df571c6582341a1160f444b5816f0a26382ed
0xcacti <0xcacti@gmail.com>
2024-08-15 00:21:40 -0400

    will need to have some sort of memoization

--- lisp/012.lisp
+++ lisp/012.lisp
@@ -1,12 +1,16 @@
 ;; What is the value of the first triangle number to have over five hundred divisors?
 
 (defun trianglep (number)
-  (when (> number 1)
-    (let ((sum 0))
-      (loop from 1 to (
+  "Return t if number is a triangle number, nil otherwise."
+  (let ((sum 0))
+    (loop for i from 1 to number do
+          (setf sum (+ sum i))
+          (when (= sum number)
+            (return t)))
+    nil))
 
-(defun next-prime (number)
-  (loop for n from number when (primep n) return n))
+(defun next-triangle (number)
+  (loop for n from number when (trianglep n) return n))
 
 
 
